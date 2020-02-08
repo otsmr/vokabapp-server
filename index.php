e37d66e18b8c5d21fc850d056b823d6b29e77e68
tsmr <you@tsmr.eu>
2020-02-08 16:31:51 +0100

    Webseite verschönert

--- index.php
+++ index.php
@@ -2,19 +2,58 @@
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>VokabApp Server</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <title>VokabApp</title>
+    <link rel="stylesheet" href="assets/css/web.css">
+    <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
 </head>
 <body>
-    <main>
-        <header>
-            <h1>VokabApp Server</h1>
-            <p class='desc'>Ein Service von <a href='https://oproj.de'>oproj.de.</a></p>
-        </header>
-        <a class='button' href="https://github.com/otsmr/vokabapp">zu Github</a>
-        <a class='imprint' href='https://oproj.de/privacy'>Privacy & Imprint</a>
+
+    <header>
+        <nav>
+            <ul>
+                <a href="https://beta.odmin.de"><li>Anmelden</li></a>
+                <a href="https://beta.odmin.de"><li class="border">Registrieren</li></a>
+                <a href="https://oproj.de/imprint"><li>Impressum</li></a>
+            </ul>
+        </nav>
+        <div class="group">
+            
+            <div class="text">
+                <div class="slogan">
+                    <img src="assets/img/logo.png">
+                    <p>
+                        Offline. Überall.<br>
+                        Vokabeln lernen.
+                    </p>
+                    <a href="https://github.com/otsmr/vokabapp">
+                        <button class="button">
+                            zu Github
+
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div class="screenshot">
+                <img src="assets/img/screenshot.png">
+            </div>
+        </div>
+    </header>
+    <main class="group">
+        <div>
+            <img src="assets/img/history.svg">
+            <p>Synchronisieren</p>
+        </div>
+        <div>
+            <img src="assets/img/code.svg" style="transform: scale(1.5);">
+            <p>Open Source</p>
+        </div>
+        <div>
+            <img src="assets/img/cross-platform.svg" style="transform: scale(.85);">
+            <p>Cross-Plattform</p>
+        </div>
     </main>
+    
 </body>
 </html>
